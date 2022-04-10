51bf0c03b3b280785f7bf1489a0cff0d4d16c293
효주 이 <gywn7416@pukyong.ac.kr>
2022-04-11 00:57:44 +0900

    초기 커밋.

---
+++ CSharpStudy/Object/ClassOne.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpStudy.Object
+{
+    public class ClassOne
+    {
+        public static void Hi() => Console.WriteLine("Hi");
+    }
+    public class ClassTwo
+    {
+        public void Hi() => Console.WriteLine("Hi");
+    }
+}
+
+
