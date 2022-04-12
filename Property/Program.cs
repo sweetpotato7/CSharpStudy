9e287020ad5988c6a95d2c796a184f2b65dc2c68
효주 이 <gywn7416@pukyong.ac.kr>
2022-04-12 23:43:54 +0900

    속성

---
+++ Property/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Property
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Name = "My Car";
+
+            // car.Age = "21"; // 읽기 전용이므로 불가
+        }
+    }
+}
---
+++ Property/car.cs
@@ -0,0 +1,11 @@
+﻿namespace Property
+{
+    partial class Program
+    {
+        class Car
+        {
+            public string Name { get; set; } // 이를 통해 public한 Name 속성으로 외부에 공개됨
+            public string Age { get; private set; } = "28"; // 읽는 건 허용되지만 쓰는건 불가(읽기전용)
+        }
+    }
+}
