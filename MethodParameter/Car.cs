8ca80200f809a922f359e8addd8ee90f3ef62fbf
효주 이 <gywn7416@pukyong.ac.kr>
2022-04-12 23:31:56 +0900

    메서드와 매개변수

---
+++ MethodParameter/Car.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace MethodParameter
+{
+    class Car
+    {
+        public void Map(ref string title) => Console.WriteLine($"{title}"); // ref = 참조
+    }
+}
---
+++ MethodParameter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string title = "복사"; Console.WriteLine($"{title}");
+            var car = new Car(); car.Map(ref title);
+            Console.WriteLine($"{title}");
+        }
+    }
+}
