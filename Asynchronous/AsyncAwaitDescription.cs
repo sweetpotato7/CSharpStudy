292616e514bdcd2e29bdbb9a7d08e137df6ad443
효주 이 <gywn332@micube.co.kr>
2022-05-06 17:53:45 +0900

    비동기

---
+++ Asynchronous/AsyncAwaitDescription.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asynchronous
+{
+    class AsyncAwaitDescription
+    {
+        static void Main()
+        {
+            Task.Run(() => PrintNumberAsync());
+        }
+
+        static void PrintNumber() // 동기 방식
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine(i + 1);
+            }
+        }
+
+        static async Task PrintNumberAsync()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 30; i++)
+                {
+                    Console.WriteLine(i + 1);
+                }
+            });
+        }
+    }
+}
---
+++ Asynchronous/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Asynchronous
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await DoAsync();
+        }
+
+        static async Task DoAsync() // 비동기 메서드
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("http://www.naver.com");
+                var content = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
